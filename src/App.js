114d607a59e9a613acd7fd0edc3c832141ad3f1f
rickyjoe11 <syrus.samson99@gmail.com>
2023-05-19 09:42:05 -0400

    init repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import img1 from './imgs/mock.png'
+import img2 from './imgs/mock2.png'
+import react, {useState, useEffect} from 'react';
+
 
 function App() {
+
+const [img, setImg] = useState(img2);
+
+const handleImg = () => {
+  if (img === img1) {
+    setImg(img2);
+  } else {
+    setImg(img1);
+  }
+};
+
+const handleTimeout = (fn) => {
+  setTimeout(fn, 3000);
+};
+
+useEffect(() => {
+  handleTimeout(handleImg); // Pass handleImg without parentheses
+}, [img]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div>
+  <div className='home-container '>
+    <div className='container-fluid px-5 home-container-inner'> 
+      <div className='row home-container-inner '>
+        <div className='col-12 col-lg-6'>
+          <div className='img-container' style={{backgroundImage: `url(${img})`}}>
+          <div className='img-overlay-box'>
+          <div className="content-wrapper p-3 img-overlay-box-inner">
+          <h1 className='my-0'>Hello, I'm Nikola, and <br></br><a>I design experience!</a></h1>
+          <p className='text-muted'>Based in: Zagreb, Croatia</p>
+          </div>
+          </div>
+          </div>
+        </div>
+        <div className='col-12 col-lg-6 text-container-outer d-flex flex-column align-items-center'>
+
+            <div className='text-container-inner px-3'>
+              <p className='home-text-large'>I'm a passionate UX/UI professional with 12 years of experience in tech and over 6 years in UX research, with a background in graphic design (branding / creating marketing assets / DTP / social media / packaging) for different B2B or B2C products.
+              </p>
+              <br></br>
+              <p className='home-text-supporting my-3'>I can provide you with <b>Employee Experience</b> and <b>consistency</b> based on new or current designs <b>through all platforms,</b> and across all <b>marketing materials.</b>
+              </p>
+              <br></br>
+              <p className='home-text-supporting my-3'>
+              Strongly focused on user experience <b>research</b>, human behavior, and <b>psychology</b> as well.
+              </p>
+              <br></br>
+               <p className='home-text-supporting my-3'>I’m a <b>product nerd</b> with a focused goal of improving people's lives through simple user-focused design solutions.
+              </p>
+              <br></br>
+              <p className='home-text-supporting my-3'>
+              By improving people’s life I mean the<b> value of their time</b>. <b>Currently looking for new opportunities</b>. If you have questions please feel free to contact me at any time.
+              </p>
+              </div>
+              <div className='nav-container mt-auto me-auto px-3'>
+
+              <a className='navlink-item  '><p className='fixed'>WORKING EXPERIENCE</p></a><br></br>
+              <a className='navlink-item'><p className='fixed'>CASE STUDIES</p></a><br></br>
+              <a className='navlink-item'><p className='fixed'>SKILLS</p></a>
+
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+</div>
   );
 }
 
